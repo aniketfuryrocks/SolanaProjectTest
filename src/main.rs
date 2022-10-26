427c6a273cfde59d6e01f6755533bef6b01372d1
aniketfuryrocks <contact@aniketprajapati.me>
2022-10-26 10:12:49 +0530

    format

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::net::SocketAddr;
 use std::sync::Arc;
 
-use borsh::{BorshSerialize, BorshDeserialize};
+use borsh::{BorshDeserialize, BorshSerialize};
 use solana_client::connection_cache;
-use solana_client::nonblocking::tpu_client::TpuClient;
 use solana_client::nonblocking::rpc_client::RpcClient;
+use solana_client::nonblocking::tpu_client::TpuClient;
 use solana_client::thin_client::ThinClient;
 use solana_client::tpu_client::TpuClientConfig;
 
@@ -16,7 +16,6 @@ use solana_sdk::signature::Keypair;
 use solana_sdk::signer::Signer;
 use solana_sdk::transaction::Transaction;
 
-
 #[derive(BorshSerialize, BorshDeserialize)]
 enum BankInstruction {
     Initialize,
@@ -24,55 +23,43 @@ enum BankInstruction {
     Withdraw { lamports: u64 },
 }
 
-async fn tpuclientsendtransaction(program_id: Pubkey, payer: &Keypair){
-    let c=RpcClient::new("http://0.0.0.0:8899/".to_string());
-    let wurl="ws://localhost:8900";
-    let client=TpuClient::new(Arc::new(c), wurl, TpuClientConfig::default()).await.unwrap();
-    
-    let bankins=BankInstruction::Initialize;
-    let instruction = Instruction::new_with_borsh(
-        program_id,
-        &bankins,
-        vec![],
-    );
+async fn tpuclientsendtransaction(program_id: Pubkey, payer: &Keypair) {
+    let c = RpcClient::new("http://0.0.0.0:8899/".to_string());
+    let wurl = "ws://localhost:8900";
+    let client = TpuClient::new(Arc::new(c), wurl, TpuClientConfig::default())
+        .await
+        .unwrap();
 
-    let message = Message::new(
-        &[instruction],
-        Some(&payer.pubkey()),
-    );
+    let bankins = BankInstruction::Initialize;
+    let instruction = Instruction::new_with_borsh(program_id, &bankins, vec![]);
+
+    let message = Message::new(&[instruction], Some(&payer.pubkey()));
     let blockhash = client.rpc_client().get_latest_blockhash().await.unwrap();
     let tx = Transaction::new(&[payer], message, blockhash);
-    
-    let x=client.send_transaction(&tx).await;
-    println!("{}",x);
+
+    let x = client.send_transaction(&tx).await;
+    println!("{}", x);
 }
 
-fn thinclientsendtransaction(program_id: Pubkey, payer: &Keypair){
-    let rpc_addr="127.0.0.1:8899".parse::<SocketAddr>().unwrap();
-    let tpu_addr="127.0.0.1:1027".parse::<SocketAddr>().unwrap();
-    let connectioncache=connection_cache::ConnectionCache::new(20000000);
-    let client=ThinClient::new(rpc_addr, tpu_addr, Arc::new(connectioncache));
-    let bankins=BankInstruction::Initialize;
-    let instruction = Instruction::new_with_borsh(
-        program_id,
-        &bankins,
-        vec![],
-    );
+fn thinclientsendtransaction(program_id: Pubkey, payer: &Keypair) {
+    let rpc_addr = "127.0.0.1:8899".parse::<SocketAddr>().unwrap();
+    let tpu_addr = "127.0.0.1:1027".parse::<SocketAddr>().unwrap();
+    let connectioncache = connection_cache::ConnectionCache::new(20000000);
+    let client = ThinClient::new(rpc_addr, tpu_addr, Arc::new(connectioncache));
+    let bankins = BankInstruction::Initialize;
+    let instruction = Instruction::new_with_borsh(program_id, &bankins, vec![]);
 
-    let message = Message::new(
-        &[instruction],
-        Some(&payer.pubkey()),
-    );
+    let message = Message::new(&[instruction], Some(&payer.pubkey()));
     let blockhash = client.rpc_client().get_latest_blockhash().unwrap();
     let tx = Transaction::new(&[payer], message, blockhash);
-    let x=client.async_send_transaction(tx).unwrap();
+    let x = client.async_send_transaction(tx).unwrap();
     //let x=client.get_account(&program_id).unwrap();
-    println!("{:#?}",x);
+    println!("{:#?}", x);
 }
 
 fn main() {
-    let program_id=Pubkey::new_unique();
-    let payer=Keypair::new();
+    let program_id = Pubkey::new_unique();
+    let payer = Keypair::new();
     let rt = tokio::runtime::Runtime::new().unwrap();
     rt.block_on(tpuclientsendtransaction(program_id, &payer));
     thinclientsendtransaction(program_id, &payer);
